2ddd92122ac215beb6f4fc97283125e4912303b8
sakibcd097@gmail.com <sakibcd097@gmail.com>
2024-03-20 23:15:36 +0800

    user login added

--- Day-05 URL and VIews/my_tennis_club/members/views.py
+++ Day-05 URL and VIews/my_tennis_club/members/views.py
@@ -75,3 +75,18 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid login credentials'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
