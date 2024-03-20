abd6b5a556a1af22fa7adaf60a420efb19417733
sakibcd097@gmail.com <sakibcd097@gmail.com>
2024-03-20 23:06:50 +0800

    updated converter

--- Day-05 URL and VIews/my_tennis_club/members/views.py
+++ Day-05 URL and VIews/my_tennis_club/members/views.py
@@ -8,3 +8,17 @@ def name(request):
 
 def cake(request):
     return HttpResponse('This is my cake')
+
+
+
+def emoji_converter(request):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        converted_text = ""
+        for char in text:
+            if char.isalpha():
+                converted_text += chr(ord(char) + 128512)  # Convert to emoji code point
+            else:
+                converted_text += char
+        return render(request, 'emoji_converter.html', {'converted_text': converted_text})
+    return render(request, 'emoji_converter.html')
