d58d73e4ac9842f298273502fe6455722fa5776d
sakibcd097@gmail.com <sakibcd097@gmail.com>
2024-03-23 13:14:41 +0800

    htmtl connected

--- Day-06/my_club/product/urls.py
+++ Day-06/my_club/product/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.cake),
+    path('tem/',views.cake),
   
 ]
--- Day-06/my_club/product/views.py
+++ Day-06/my_club/product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 def cake(request):
-    return HttpResponse('This is my cake')
+    return render(request,'product.html')
