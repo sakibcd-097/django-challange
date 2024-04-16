3a984e3ea1392e7584e555017eddba6aa5ac67b7
sakibcd097@gmail.com <sakibcd097@gmail.com>
2024-04-16 14:18:32 +0800

    updated

--- Step-01/my_first_project/my_first_project/urls.py
+++ Step-01/my_first_project/my_first_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for my_app project.
+URL configuration for my_first_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('first/',views.index, name='index'),
+    path('',views.index, name='index' ),
 ]
