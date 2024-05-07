ed5191bfecd9e8103031d2921155f27b242e0afc
sakibcd097@gmail.com <sakibcd097@gmail.com>
2024-05-07 14:16:59 +0800

    updated

--- Step-03/my_project/first_app/views.py
+++ Step-03/my_project/first_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("This is home page")
+
+def contact(request):
+    return HttpResponse("This is contact page")
+
