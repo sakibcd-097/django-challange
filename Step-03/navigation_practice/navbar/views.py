16c7d2d7749e6e1bb1aa1ae98c2d44dc1d2d08a3
sakibcd097@gmail.com <sakibcd097@gmail.com>
2024-05-16 17:30:13 +0800

    backend data passed

--- Step-03/navigation_practice/navbar/views.py
+++ Step-03/navigation_practice/navbar/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
 def about(request):
-    return render(request,"navbar/about.html")
+    d={'name':'Rahim', 'age':25, 'courses':[
+        {
+            'id':1,
+            'name':'Sakib',
+            'roll':25
+        },
+        {
+            'id':2,
+            'name':'Rahim',
+            'roll':24
+            
+        }
+        ]}
+    return render(request,"navbar/about.html",d)
 def contact(request):
     return render(request,"navbar/contact.html")
